81c143a95543fb24618ea4e22c5e9449bf17546a
QuiD-0 <wodnd101@naver.com>
2023-04-28 17:23:57 +0900

    init project

---
+++ src/main/kotlin/com/quid/reviews/ReviewsApplication.kt
@@ -0,0 +1,11 @@
+package com.quid.reviews
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviewsApplication>(*args)
+}
---
+++ src/test/kotlin/com/quid/reviews/ReviewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quid.reviews
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
