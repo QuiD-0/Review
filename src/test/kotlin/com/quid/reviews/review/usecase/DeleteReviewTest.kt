f41107b60ea0dc232ced6380ffe6d48a199f7584
QuiD-0 <wodnd101@naver.com>
2023-05-12 11:17:39 +0900

    리뷰 삭제 테스트

---
+++ src/test/kotlin/com/quid/reviews/review/usecase/DeleteReviewTest.kt
@@ -0,0 +1,39 @@
+package com.quid.reviews.review.usecase
+
+import com.quid.reviews.review.gateway.web.ReviewCreateRequest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
+
+@Profile
+@SpringBootTest
+class DeleteReviewTest{
+
+    @Autowired
+    lateinit var deleteReview: DeleteReview
+
+    @Autowired
+    lateinit var createReview: CreateReview
+
+    lateinit var id: String
+
+    @BeforeEach
+    fun setup(){
+        val request = ReviewCreateRequest(
+            title = "title",
+            description = "description",
+            score = 5,
+            author = "author",
+            productId = 1
+        )
+        id = createReview.create(request).id!!
+    }
+
+    @Test
+    fun `리뷰 삭제`(){
+        assertDoesNotThrow { deleteReview.delete(id) }
+    }
+}
