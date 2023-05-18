b667a008ab7c0071fc62313878342e62b6eddf6b
QuiD-0 <wodnd101@naver.com>
2023-05-18 15:10:06 +0900

    이미지 의존성이 있는 테스트 비활성화

--- src/test/kotlin/com/quid/reviews/image/gateway/message/producer/ImageProducerTest.kt
+++ src/test/kotlin/com/quid/reviews/image/gateway/message/producer/ImageProducerTest.kt
@@ -1,14 +1,25 @@
 package com.quid.reviews.image.gateway.message.producer
 
+import com.quid.reviews.review.domain.createReview
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ImageProducerTest(@Autowired val imageProducer: ImageProducer){
+class ImageProducerTest(@Autowired val imageProducer: ImageProducer) {
 
     @Test
-    fun producerTest(){
-        imageProducer.compress("test")
+    @Disabled
+    fun producerTest() {
+        val review = createReview(
+            title = "test",
+            description = "test",
+            rating = 1,
+            productId = 1,
+            author = "test",
+            imgList = listOf()
+        )
+        imageProducer.compress(review)
     }
 }
